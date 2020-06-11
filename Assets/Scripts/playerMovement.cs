5a2dd147aeb706bf804b9ecab5bbf598bbfbacf5
yes99 <yes950324@naver.com>
2020-06-12 05:34:58 +0900

    기본 스크립트 작성
    오른쪽 왼쪽 움직이는거 로그 확인

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    Vector3 movement;
+    Quaternion Rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("왼쪽으로 이동 중");
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("오른쪽 이동 중");
+        }
+
+
+    }
+}
