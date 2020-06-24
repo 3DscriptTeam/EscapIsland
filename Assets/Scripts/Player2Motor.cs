78a682d0cfc996e491cebbad4cda0696d3ef9a77
yes99 <yes950324@naver.com>
2020-06-24 19:47:09 +0900

    캐릭터& 카메라 재구축중
    완전 새로 다시
    무한 러닝 입각하ㅕㅇ

--- Assets/Scripts/Player2Motor.cs
+++ Assets/Scripts/Player2Motor.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Player2Motor : MonoBehaviour
 {
     private CharacterController controller;
+    private Vector3 moveVector;
+    
     private float speed = 5.0f;
-
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,26 @@ public class Player2Motor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveVector = Vector3.zero;
+
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity* Time.deltaTime;
+        }
+
+
+
+        //X - 왼쪽 오른쪽
+        moveVector.x = Input.GetAxisRaw("Horizontal")* speed;
+        //Y - 위 아래
+        moveVector.y = verticalVelocity;
+        //Z-  앞뒤이긴 한데 여기선 안쓰지
+        moveVector.z = speed;
+
         controller.Move(Vector3.forward * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+
+    private int desiredLane = 1; // 0 왼쪽 , 1 가운데 2 오른쪽
+    public float laneDistance = 4; // the distance between two lane
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+        // 우리가 있어야 할 라인
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if(desiredLane == 3)
+            {
+                desiredLane = 2;
+            }    
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+
+        }
+        
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if(desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+
+        }
+        else if(desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+       transform.position = targetPosition;
+        //Vector3.Lerp(transform.position, targetPosition, 80 * Time.deltaTime);
+
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+}
