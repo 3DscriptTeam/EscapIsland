d42bd3791db406c9fad0cc59fdc2f988dad4f219
yes99 <yes950324@naver.com>
2020-06-25 23:49:13 +0900

    타이머 유아이 성공
    타이머 유아이 성공

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text _text;
+    float timeleft = 100;
+   
+
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        _text.text = "TIME LEFT : " + timeleft;
+        if (timeleft <= 0 )
+        {
+          
+            timeleft = 0;
+        }
+
+    }
+
+}
+
+
+
+
+
