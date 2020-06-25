6960d91e774f94e5f4f13da784388d257c178215
yes99 <yes950324@naver.com>
2020-06-26 02:36:23 +0900

    아이템 첫 시도
    그린 포션까지 성공

---
+++ Assets/Scripts/_item/ItemDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public static ItemDatabase instance;
+    public List<Item> items = new List<Item>();
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Add("GreenPotion", 1, 500, "green potion", ItemType.Consumption);
+    }
+
+    void Add(string itemName, int itemValue, int itemPrice, string itemDesc, ItemType itemType)
+    {
+        items.Add(new Item(itemName, itemValue, itemPrice, itemDesc, itemType, Resources.Load<Sprite>("ItemImages/" + itemName)));
+    }
+}
